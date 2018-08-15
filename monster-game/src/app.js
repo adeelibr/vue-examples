c6478dc45252be6fd6f928020043ce3675099d39
adeelibr <adeelimranr@gmail.com>
2018-08-15 23:49:31 +0500

    game refactor

--- monster-game/src/app.js
+++ monster-game/src/app.js
@@ -11,10 +11,45 @@ new Vue({
     }, 2000);
   },
   methods: {
-    onStartGame: function () {
+    onStartGame: function() {
       this.isGameRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    onAttack: function() {
+      this.monsterHealth -= this.onCalculateDamage({ max: 10, min: 3 });
+      if (this.isGameWon()) { return }
+      this.playerHealth -= this.onCalculateDamage({ max: 12, min: 5 });
+      this.isGameWon();
+    },
+    onSpecialAttack: function() {},
+    onHeal: function() {},
+    onGiveUp: function() {},
+
+    /**
+     * Helpers
+     */
+    onCalculateDamage: function({ max, min }) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    isGameWon: function () {
+      if (this.monsterHealth <= 0) {
+        if(confirm('You won! Play another?')) {
+          this.onStartGame();
+        } else {
+          this.isGameRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if(confirm('You Lost! Play another?')) {
+          this.onStartGame();
+        } else {
+          this.isGameRunning = false;
+        }
+        return true;
+      } else {
+        return false;
+      }
     },
   }
 })
